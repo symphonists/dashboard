07a66eb6141b9c310c02966ea9b4811e27f26cc6
Nick Dunn <nick@nickdunn.local>
2010-08-02 08:55:03 +0100

    JS refactoring

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -1,12 +1,18 @@
 var Dashboard = {
 	
+	$dashboard: null,
+	$h2: null,
+	
 	init: function() {
 		var self = this;
+		
+		this.$dashboard = jQuery('#dashboard');
+		this.$h2 = jQuery('h2:first');
 		
 		// Create New button
 		jQuery('a.create').bind('click', function(e) {
 			e.preventDefault();		
-			var panel_type = jQuery('h2 select').val();
+			var panel_type = self.$h2.find('select').val();
 			self.showEditForm(panel_type);
 		});
 
@@ -21,8 +27,7 @@ var Dashboard = {
 		// Delete panel button
 		jQuery('#save-panel button[name="action[delete]"]').live('click', function(e) {
 			e.preventDefault();
-			var data = jQuery('form').serialize() + '&action[delete]=true';
-			self.savePanel(data, 'delete');
+			self.savePanel(jQuery('form').serialize(), 'delete');
 		});
 		
 		// Cancel form button
@@ -34,15 +39,13 @@ var Dashboard = {
 		// Save panel button
 		jQuery('#save-panel input[name="action[submit]"]').live('click', function(e) {
 			e.preventDefault();
-			var data = jQuery('form').serialize() + '&action[submit]=true';
-			self.savePanel(data, 'save');
+			self.savePanel(jQuery('form').serialize(), 'submit');
 		});
 		
 		// Save panel button (form submit default)
 		jQuery('form').bind('submit', function(e) {
 			e.preventDefault();
-			var data = jQuery('form').serialize() + '&action[submit]=true';
-			self.savePanel(data);
+			self.savePanel(jQuery('form').serialize(), 'submit');
 		});
 		
 		jQuery('.primary, .secondary').sortable({
@@ -51,23 +54,8 @@ var Dashboard = {
 			placeholder: 'panel-placeholder',
 			handle: '> h3',
 			revert: 200,
-			stop: function(event, ui) {
-				var post = '';
-				var i = 0;
-				jQuery('.primary, .secondary').each(function(j) {
-					var sort_order = 1;
-					jQuery('.panel', this).each(function() {
-						post += 'panel[' + i + '][id]=' + jQuery(this).attr('id').replace(/^id-/,'') + '&';
-						post += 'panel[' + i + '][placement]=' + ((j == 0) ? 'primary' : 'secondary') + '&';
-						post += 'panel[' + i + '][sort_order]=' + sort_order++ + '&';
-						i++;
-					});
-				});
-				jQuery.ajax({
-					type: 'POST',
-					url: Symphony.WEBSITE + '/symphony/extension/dashboard/save_order/',
-					data: post,
-				});
+			stop: function() {
+				self.saveReordering();
 			}
 		});
 
@@ -78,10 +66,31 @@ var Dashboard = {
 		
 	},
 	
+	saveReordering: function() {
+		
+		var post_data = '';
+		var i = 0;
+		
+		jQuery('.primary, .secondary').each(function(j) {
+			var sort_order = 1;
+			jQuery('.panel', this).each(function() {
+				post_data += 'panel[' + i + '][id]=' + jQuery(this).attr('id').replace(/^id-/,'') + '&';
+				post_data += 'panel[' + i + '][placement]=' + ((j == 0) ? 'primary' : 'secondary') + '&';
+				post_data += 'panel[' + i + '][sort_order]=' + sort_order++ + '&';
+				i++;
+			});
+		});
+		
+		jQuery.ajax({
+			type: 'POST',
+			url: Symphony.WEBSITE + '/symphony/extension/dashboard/save_order/',
+			data: post_data,
+		});
+		
+	},
+	
 	hideEditForm: function(callback, enable_dashboard) {
-		if (enable_dashboard === true) {
-			jQuery('#dashboard').fadeTo('fast', 1);
-		}
+		if (enable_dashboard === true) this.$dashboard.fadeTo('fast', 1);
 		jQuery('#save-panel').slideUp(function() {
 			jQuery(this).remove();
 			if (typeof callback == 'function') callback();
@@ -95,11 +104,14 @@ var Dashboard = {
 			url: Symphony.WEBSITE + '/symphony/extension/dashboard/panel_config/?type=' + panel_type + ((id != null) ? ('&id=' + id) : ''),
 			success: function(data) {
 				var form = jQuery('#save-panel');
-				// if form exists in the DOM it needs to be removed first before the new form is revealed
+				// if form exists in the DOM it needs to be removed first
+				// before the new form is revealed
 				if (form.length) {
-					self.hideEditForm(function() { self.revealEditForm(data); }, false);
+					self.hideEditForm(function() {
+						self.revealEditForm(data);
+					}, false);
 				} else {
-					self.revealEditForm(data, false);
+					self.revealEditForm(data);
 				}
 			}
 		});
@@ -107,36 +119,43 @@ var Dashboard = {
 	
 	revealEditForm: function(html) {
 		// fade down dashboard panels to give edit form more priority
-		jQuery('#dashboard').fadeTo('fast', 0.5);
+		this.$dashboard.fadeTo('fast', 0.5);
 		// append form to page (hidden with CSS)
-		jQuery('h2').after(html);
+		this.$h2.after(html);
 		jQuery('#save-panel').slideDown();
 	},
 	
 	savePanel: function(post_data, action) {
 		var self = this;
 		
+		post_data += '&action[' + action + ']=true';
+		
 		jQuery.ajax({
 			type: 'POST',
 			url: Symphony.WEBSITE + '/symphony/extension/dashboard/panel_config/',
 			data: post_data,
 			success: function(data) {
+				
+				var id = jQuery('response', data).attr('id');
+				var placement = jQuery('response', data).attr('placement');
+				
+				var html = jQuery('response', data).text();
+				var panel = jQuery('#id-' + id);
+				
 				switch (action) {
+					
 					case 'delete':
-						var id = jQuery('response', data).attr('id');
 						self.hideEditForm(function() {
-							jQuery('#id-' + id).slideUp('slow', function() {
+							panel.slideUp('slow', function() {
 								jQuery(this).remove();
 							});
 						}, true);
 					break;
-					case 'save':
-						var id = jQuery('response', data).attr('id');
-						var placement = jQuery('response', data).attr('placement');
+					
+					case 'submit':
 						// insert new panel
 						if (id == '') {
 							self.hideEditForm(function() {
-								var html = jQuery('response', data).text();
 								jQuery('.' + placement).append(html);
 								jQuery('.new-panel').slideDown('fast', function() {
 									jQuery(this).removeClass('new-panel');
@@ -146,11 +165,9 @@ var Dashboard = {
 						// update
 						else {
 							self.hideEditForm(function() {
-								var html = jQuery('response', data).text();
-								var panel = jQuery('#id-' + id);
 								var column = panel.parent();
 								var column_name = ((panel.parent().hasClass('primary')) ? 'primary' : 'secondary');
-								jQuery('#id-' + id).fadeOut('fast', function() {
+								panel.fadeOut('fast', function() {
 									if (placement == column_name) {
 										jQuery(this).after(html).remove();
 									} else {
