4cbe4b82ee220012758cd38d703357f0bb7c1be1
Nick Dunn <nick@nickdunn.local>
2010-08-02 08:36:23 +0100

    JS OO-ified

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -1,48 +1,84 @@
-jQuery(document).ready(function() {
+var Dashboard = {
 	
-	/* 
-		* rewrite as a clean object rather than standalone functions
-		* refine selectors to prevent re-selection
-		* allow toggling of Edit controls
-	*/
+	init: function() {
+		var self = this;
+		
+		// Create New button
+		jQuery('a.create').bind('click', function(e) {
+			e.preventDefault();		
+			var panel_type = jQuery('h2 select').val();
+			self.showEditForm(panel_type);
+		});
+
+		// Edit panel button
+		jQuery('.panel a.panel-edit').live('click', function(e) {
+			e.preventDefault();		
+			var id = jQuery(this).parent().attr('id').replace(/id-/,'');
+			var panel_type = jQuery(this).parent().attr('class').replace(/panel /,'');
+			self.showEditForm(panel_type, id);
+		});
+
+		// Delete panel button
+		jQuery('#save-panel button[name="action[delete]"]').live('click', function(e) {
+			e.preventDefault();
+			var data = jQuery('form').serialize() + '&action[delete]=true';
+			self.savePanel(data, 'delete');
+		});
+		
+		// Cancel form button
+		jQuery('#save-panel input[name="action[cancel]"]').live('click', function(e) {
+			e.preventDefault();
+			self.hideEditForm(null, true);
+		});
+		
+		// Save panel button
+		jQuery('#save-panel input[name="action[submit]"]').live('click', function(e) {
+			e.preventDefault();
+			var data = jQuery('form').serialize() + '&action[submit]=true';
+			self.savePanel(data, 'save');
+		});
+		
+		// Save panel button (form submit default)
+		jQuery('form').bind('submit', function(e) {
+			e.preventDefault();
+			var data = jQuery('form').serialize() + '&action[submit]=true';
+			self.savePanel(data);
+		});
+		
+		jQuery('.primary, .secondary').sortable({
+			items: '.panel',
+			connectWith: '.sortable-container',
+			placeholder: 'panel-placeholder',
+			handle: '> h3',
+			revert: 200,
+			stop: function(event, ui) {
+				var post = '';
+				var i = 0;
+				jQuery('.primary, .secondary').each(function(j) {
+					var sort_order = 1;
+					jQuery('.panel', this).each(function() {
+						post += 'panel[' + i + '][id]=' + jQuery(this).attr('id').replace(/^id-/,'') + '&';
+						post += 'panel[' + i + '][placement]=' + ((j == 0) ? 'primary' : 'secondary') + '&';
+						post += 'panel[' + i + '][sort_order]=' + sort_order++ + '&';
+						i++;
+					});
+				});
+				jQuery.ajax({
+					type: 'POST',
+					url: Symphony.WEBSITE + '/symphony/extension/dashboard/save_order/',
+					data: post,
+				});
+			}
+		});
+
+		jQuery('.primary, .secondary').droppable({
+			activeClass: 'hover',
+			hoverClass: 'active'	
+		});
+		
+	},
 	
-	jQuery('a.create').bind('click', function(e) {
-		e.preventDefault();		
-		var type = jQuery('h2 select').val();
-		showEditForm(null, type);
-	});
-	
-	jQuery('.panel a.panel-edit').live('click', function(e) {
-		e.preventDefault();		
-		var id = jQuery(this).parent().attr('id').replace(/id-/,'');
-		var type = jQuery(this).parent().attr('class').replace(/panel /,'');
-		showEditForm(id, type);
-	});
-	
-	jQuery('#save-panel button[name="action[delete]"]').live('click', function(e) {
-		e.preventDefault();
-		var data = jQuery('form').serialize() + '&action[delete]=true';
-		savePanel(data, 'delete');
-	});
-	
-	jQuery('#save-panel input[name="action[cancel]"]').live('click', function(e) {
-		e.preventDefault();
-		hideEditForm(null, true);
-	});
-	
-	jQuery('#save-panel input[name="action[submit]"]').live('click', function(e) {
-		e.preventDefault();
-		var data = jQuery('form').serialize() + '&action[submit]=true';
-		savePanel(data, 'save');
-	});
-	
-	jQuery('form').bind('submit', function(e) {
-		e.preventDefault();
-		var data = jQuery('form').serialize() + '&action[submit]=true';
-		savePanel(data);
-	});
-	
-	function hideEditForm(callback, enable_dashboard) {
+	hideEditForm: function(callback, enable_dashboard) {
 		if (enable_dashboard === true) {
 			jQuery('#dashboard').fadeTo('fast', 1);
 		}
@@ -50,33 +86,36 @@ jQuery(document).ready(function() {
 			jQuery(this).remove();
 			if (typeof callback == 'function') callback();
 		});
-	}
+	},
 	
-	function showEditForm(id, type) {
+	showEditForm: function(panel_type, id) {
+		var self = this;
 		jQuery.ajax({
 			type: 'GET',
-			url: Symphony.WEBSITE + '/symphony/extension/dashboard/panel_config/?type=' + type + ((id != null) ? ('&id=' + id) : ''),
+			url: Symphony.WEBSITE + '/symphony/extension/dashboard/panel_config/?type=' + panel_type + ((id != null) ? ('&id=' + id) : ''),
 			success: function(data) {
 				var form = jQuery('#save-panel');
 				// if form exists in the DOM it needs to be removed first before the new form is revealed
 				if (form.length) {
-					hideEditForm(function() { revealEditForm(data); }, false);
+					self.hideEditForm(function() { self.revealEditForm(data); }, false);
 				} else {
-					revealEditForm(data, false);
+					self.revealEditForm(data, false);
 				}
 			}
 		});
-	}
+	},
 	
-	function revealEditForm(html) {
+	revealEditForm: function(html) {
 		// fade down dashboard panels to give edit form more priority
 		jQuery('#dashboard').fadeTo('fast', 0.5);
 		// append form to page (hidden with CSS)
 		jQuery('h2').after(html);
 		jQuery('#save-panel').slideDown();
-	}
+	},
 	
-	function savePanel(post_data, action) {
+	savePanel: function(post_data, action) {
+		var self = this;
+		
 		jQuery.ajax({
 			type: 'POST',
 			url: Symphony.WEBSITE + '/symphony/extension/dashboard/panel_config/',
@@ -85,8 +124,8 @@ jQuery(document).ready(function() {
 				switch (action) {
 					case 'delete':
 						var id = jQuery('response', data).attr('id');
-						hideEditForm(function() {
-							jQuery('#id-' + id).fadeOut('slow', function() {
+						self.hideEditForm(function() {
+							jQuery('#id-' + id).slideUp('slow', function() {
 								jQuery(this).remove();
 							});
 						}, true);
@@ -96,14 +135,17 @@ jQuery(document).ready(function() {
 						var placement = jQuery('response', data).attr('placement');
 						// insert new panel
 						if (id == '') {
-							hideEditForm(function() {
+							self.hideEditForm(function() {
 								var html = jQuery('response', data).text();
 								jQuery('.' + placement).append(html);
+								jQuery('.new-panel').slideDown('fast', function() {
+									jQuery(this).removeClass('new-panel');
+								})
 							}, true);
 						}
 						// update
 						else {
-							hideEditForm(function() {
+							self.hideEditForm(function() {
 								var html = jQuery('response', data).text();
 								var panel = jQuery('#id-' + id);
 								var column = panel.parent();
@@ -124,36 +166,9 @@ jQuery(document).ready(function() {
 			}
 		});
 	}
-		
-	jQuery('.primary, .secondary').sortable({
-		items: '.panel',
-		connectWith: '.sortable-container',
-		placeholder: 'panel-placeholder',
-		handle: '> h3',
-		revert: 200,
-		stop: function(event, ui) {
-			var post = '';
-			var i = 0;
-			jQuery('.primary, .secondary').each(function(j) {
-				var sort_order = 1;
-				jQuery('.panel', this).each(function() {
-					post += 'panel[' + i + '][id]=' + jQuery(this).attr('id').replace(/^id-/,'') + '&';
-					post += 'panel[' + i + '][placement]=' + ((j == 0) ? 'primary' : 'secondary') + '&';
-					post += 'panel[' + i + '][sort_order]=' + sort_order++ + '&';
-					i++;
-				});
-			});
-			jQuery.ajax({
-				type: 'POST',
-				url: Symphony.WEBSITE + '/symphony/extension/dashboard/save_order/',
-				data: post,
-			});
-		}
-	});
 	
-	jQuery('.primary, .secondary').droppable({
-		activeClass: 'hover',
-		hoverClass: 'active'	
-	});
-	
+};
+
+jQuery(document).ready(function() {
+	Dashboard.init();
 });
