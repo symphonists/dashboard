5c8f6572716aeb08c99d9bdb701dec48cd72511a
alexbirukov <lord_maloy@mail.ru>
2013-01-21 11:02:49 +0600

    Add Russian language
    Add Russian language file

---
+++ lang/lang.ru.php
@@ -0,0 +1,120 @@
+<?php
+
+	$about = array(
+		'name' => 'Русский',
+		'author' => array(
+			'name' => 'Александр Бирюков',
+			'email' => 'info@alexbirukov.ru',
+			'website' => 'http://alexbirukov.ru'
+		),
+		'release-date' => '2013-01-21'
+	);
+
+	/**
+	 * Dashboard
+	 */
+	$dictionary = array(
+
+		'Dashboard' => 
+		'Панель сайта',
+
+		'Untitled Panel' => 
+		'Блок без имени',
+
+		'drag to re-order' => 
+		'перетащите для сортировки',
+
+		'Data Source to Table' => 
+		'Данные в виде таблицы',
+
+		'RSS Reader' => 
+		'RSS Ридер',
+
+		'HTML Block' => 
+		'HTML блок',
+
+		'Markdown Text' => 
+		'Текст с разметкой',
+
+		'Symphony Overview' => 
+		'Основные параметры',
+
+		'Feed URL' => 
+		'URL потока',
+
+		'Items to display' => 
+		'Записи для показа',
+
+		'Full view' => 
+		'Подробный вид',
+
+		'All items' => 
+		'Все записи',
+
+		'items' => 
+		'записи',
+
+		'List view' => 
+		'В виде списка',
+
+		'Cache (minutes)' => 
+		'Кэшировать (мин.)',
+
+		'Page URL' => 
+		'URL страницы',
+
+		'Markdown Text Block' => 
+		'Текстовый блок с разметкой',
+
+		'Text' => 
+		'Текст',
+
+		'The Data Source with the name <code>%s</code> could not be found.' => 
+		'Источиник данных с именем <code>%s</code> не найден.',
+
+		'Error: could not retrieve panel XML feed.' => 
+		'Ошибка: не удаётся получить XML данные.',
+
+		'Error: could not retrieve panel HTML.' => 
+		'Ошибка: не удаётся получить HTML данные.',
+
+		'Website Name' => 
+		'Имя сайта',
+
+		'Latest is %s' => 
+		'Последние из %s',
+
+		'Configuration' => 
+		'Настройка',
+
+		'Statistics' => 
+		'Сататистика',
+
+		'Nice to meet you' => 
+		'Приятно вас видеть',
+
+		'Good morning' => 
+		'Доброе утро',
+
+		'Welcome back' => 
+		'С возвращением',
+
+		'Good evening' => 
+		'Добрый вечер',
+
+		'New Panel' => 
+		'Новая панель',
+
+		'Enable Editing' => 
+		'Вкл. редактирование',
+
+		'Disable Editing' => 
+		'Выкл. редактирование',
+
+		'Save Panel' => 
+		'Сохранить панель',
+
+		'Delete Panel' => 
+		'Удалить панель',
+
+	);
